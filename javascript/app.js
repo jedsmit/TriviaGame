72e6d43dbce218c3b979ef5765c056431dc718ad
Jed Smit <jedsmit1@gmail.com>
2019-10-14 21:33:42 -0700

    removed carousel and replaced with jumbotron. started coding js objects for questions and answers

---
+++ javascript/app.js
@@ -0,0 +1,39 @@
+
+
+
+var questions = {
+    q1: "How long is the gestation period of a goat?",
+    q2: "What do goats like to eat?",
+    q3: "How long do goats live?",
+    q4: "Which of these traits do goats have in common with humans?"
+
+
+}
+
+var answers = {
+    a1: {
+        a: "1 year",
+        b: "150 days", //correct answer
+        c: "3 hours",
+        d: "90 days"
+    },
+    a2: {
+        a: "Foraged vegetation", //correct answer
+        b: "Tin cans",
+        c: "Tires",
+        d: "All of the above!"
+    },
+    a3: {
+        a: "10-13 years",
+        b: "15-18 years", //correct answer
+        c: "7-10 years",
+        d: "Goats are immortal"
+    },
+    q4 = {
+        a: "They burp loudly when they are gassy",
+        b: "They have regional accents",
+        c: "Their offspring are called kids",
+        d: "All of the above!" // correct answer
+    },
+
+}
