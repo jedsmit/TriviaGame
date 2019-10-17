f4f6c9707575ad07dc18bf073ad15968c33eca44
Jed Smit <jedsmit1@gmail.com>
2019-10-16 23:22:37 -0700

    added interval timer to change questions

--- javascript/app.js
+++ javascript/app.js
@@ -1,31 +1,30 @@
 
 //make question and answer objects
 
-var questionObj = {
-    q1: {
+var qAndA = [
+    {
         q: "How long is the gestation period of a goat?",
         a: "1 year",
         b: "150 days", //correct answer
         c: "3 hours",
-        d: "90 days"
+        d: "90 days",
+        correct: this.b
     },
-    q2: {
+    {
         q: "What do goats like to eat?",
         a: "Foraged vegetation", //correct answer
         b: "Tin cans",
         c: "Tires",
         d: "All of the above!"
     },
-    q3: {
+    {
         q: "How long do goats live?",
         a: "10-13 years",
         b: "15-18 years", //correct answer
         c: "7-10 years",
         d: "Goats are immortal"
     },
-
-
-    q4: {
+    {
         q: "Which of these traits do goats have in common with humans?",
         a: "They burp loudly when they are gassy",
         b: "They have regional accents",
@@ -34,16 +33,33 @@ var questionObj = {
     },
 
 
-}
+];
 
 //question and answer elements
 var question = $("#q-display");
 var answerA = $("#a-display-1");
 var answerB = $("#a-display-2");
 var answerC = $("#a-display-3");
-var answerC = $("#a-display-4");
+var answerD = $("#a-display-4");
 var startButton = $("button");
 var card = $(".card");
+var userAns;
+
+
+//displays question and answers
+function showQs() {
+    question.text(qAndA[i].q);
+    answerA.text(qAndA[i].a);
+    answerB.text(qAndA[i].b);
+    answerC.text(qAndA[i].c);
+    answerD.text(qAndA[i].d);
+};
+
+//get user's answer
+
+
+
+//-----------------------------main game--------------------------------
 
 //card starts out hidden
 card.hide();
@@ -51,18 +67,39 @@ card.hide();
 //press start button to start the game
 //use jquery to display the questions and answers
 startButton.on("click", function () {
-    startButton.hide();
-    card.show();
-    i = 0;
-    setInterval(function () {
-        question.text(questionObj[i].q);
 
-    }, 10000)
-    console.log(i)
-}
-)
+    i = 0; // index of the question in the array
+    startButton.hide();
+    showQs();
+    card.show();
+
+    setInterval(function () {
+        i++;
+        showQs();
+
+
+        $("li").on("click", function () {
+            userAns = $(this).text();
+
+
+
+            if (userAns === qAndA[i].correct) {
+                $("ul").hide();
+                question.text("YAAAAAAAY");
+            } else {
+                $("ul").hide();
+                question.text("NOOOOOOOOO");
+            };
+        });
+    }, 5000);
+});
+
+
+
+
+
 
 //when user clicks an answer, replace list with correct or incorrect screen
 //incorrect screen shows the correct answer
 //after 3 seconds display the next question
-//on the final screen, show correct and incorrect count and an option to start again 
+//on the final screen, show correct and incorrect count and an option to start again
